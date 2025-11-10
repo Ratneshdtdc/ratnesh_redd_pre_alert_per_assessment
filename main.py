ba7d0586ca83dc97d6a7d30b22be30f6d66ccde1
Ratneshdtdc <146315467+Ratneshdtdc@users.noreply.github.com>
2025-11-11 01:59:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -255,13 +255,12 @@ data["Daily_Rank"] = data.groupby(rank_group)["Critical_Ratio"].rank(method="den
 # data = pd.read_csv('your_file.csv')
 
 # Ensure Date column is properly parsed
-data['Date'] = data['SheetName'].str.extract(r'A0*(\d{2})(\d{2})')[0] + '-' + data['SheetName'].str.extract(r'A0*(\d{2})(\d{2})')[1]
-data['Date'] = pd.to_datetime('2025-' + data['SheetName'].str[3:5] + '-' + data['SheetName'].str[5:7], errors='coerce')
-
-st.dataframe(
-    data['Date']
+#data['Date'] = data['SheetName'].str.extract(r'A0*(\d{2})(\d{2})')[0] + '-' + data['SheetName'].str.extract(r'A0*(\d{2})(\d{2})')[1]
+#data['Date'] = pd.to_datetime('2025-' + data['SheetName'].str[3:5] + '-' + data['SheetName'].str[5:7], errors='coerce')
+data['Date'] = pd.to_datetime(
+    '2025-' + data['_DateProxy'].str[-2:] + '-' + data['_DateProxy'].str[2:4],
+    errors='coerce'
 )
-
 # --- Streamlit layout ---
 st.set_page_config(layout="wide")
 st.title("📊 Critical Pending & Ratio Trend Dashboard")
